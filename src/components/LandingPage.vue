95dea6a5a49759328d9f96a67dd63deaf9819edb
Matei Radu <matei.radu.92@gmail.com>
2018-03-17 20:28:09 +0100

    Begins transition to new style and to Vue.js
    This commit represents the beginning of the transition to a new style
    for the website and also a technical switch from React to Vue.js.
    
    Essentially all previous files have been overwritten by the ones created
    by the command `vue init webpack` which created a ready-to-go Vue.js app
    configured with Webpack, testing frameworks and routing.

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,28 @@
+<!--
+  Copyright © 2016-2018 Matei Bogdan Radu <matei.radu.92@gmail.com>
+  Licensed under MIT
+  (https://github.com/mateiradu/personal-website/blob/master/LICENSE)
+-->
+<template>
+  <main role='main'>
+    <div class='container'>
+      <h1>Hi, I'm Matei and I'm a Software Developer</h1>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage',
+};
+</script>
+
+<style scoped>
+main {
+  flex: 1 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
