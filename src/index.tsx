043d08c43a81e9db1eb106b7be0325b7c49a9a3c
Matei Radu <matei.radu.92@gmail.com>
2018-09-29 16:30:47 +0200

    Reset application with new Preact+TS+Parcel setup
    With the objective to reboot the personal webiste, a new fresh start is
    necessary: current design, usability and technical stack are not
    appreciated anymore for this project.
    
    This commit starts fresh with a new setup using Typescript (for all the
    good reasons), Parcel (for its simplicity since this is not a complex
    project) and Preact (the fanciness of React with much smaller bundle
    size, again because this is a simple project).

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2018-present, Matei Bogdan Radu <matei.radu.92@gmail.com>
+ *
+ * This source code is licensed under the MIT license found in the LICENSE
+ * file in the root directory of this source tree.
+ */
+
+import { h, render } from "preact";
+import { App } from "./app";
+
+render(<App />, document.getElementById("app")!);
