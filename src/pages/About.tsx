48f60b884bbdcb59754f46b1efa108967dd11e1b
Matei Radu <matei.radu.92@gmail.com>
2018-10-02 19:56:13 +0200

    Add first draft of the About page content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -12,7 +12,33 @@ import { Container, Page } from "../components";
 export default (_: RoutableProps) => (
   <Page>
     <Container text>
-      <h2>About me</h2>
+      <div className="about__text">
+        <h2>About me</h2>
+        <p>
+          I am a software developer with a focus on Javascript-based
+          technologies, ranging from frontend frameworks of the likes of React
+          and Vue.js to Express.js services and vanilla Javascript tool
+          development. Typescript is my to-go option as I prefer statically
+          typed languages but I still, however, feel comfortable writing pure
+          Javascript.
+        </p>
+        <p>
+          For the last months, I have grown fond of React Native and its
+          flexibility. I keep myself involved in its development process,
+          contribute to the code base and give back to the community by
+          open-sourcing some native modules.
+        </p>
+        <p>
+          Outside of the Javascript realm, I also work with Kotlin paired with
+          Spring Boot at my current company{" "}
+          <a href="https://techdev.io/en/" target="_blank">
+            techdev Solutions
+          </a>
+          . I enjoy following industry best practices in our development
+          pipeline such as proper Git flow, coding style enforcement and scrum
+          planning. My current challenge is to make TDD second nature.
+        </p>
+      </div>
     </Container>
   </Page>
 );
