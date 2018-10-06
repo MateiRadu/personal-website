5afa45788dbe0d7d1a7035bf74abc0df2f79f5fc
Matei Radu <matei.radu.92@gmail.com>
2018-10-06 10:59:40 +0200

    Update Contact page presentation

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -14,37 +14,45 @@ export default (_: RoutableProps) => (
   <Page>
     <Container text>
       <h2>How to reach me</h2>
-      <Email address="hello@mattblock.tech" />
-      <p>You can find me on other networks.</p>
+      <p>My preferred way of being reached is though email at</p>
+      <Email address="hello@mattblock.tech" centered />
+      <p>You can also find me on other networks</p>
       <table className="contact__list">
         <tbody>
           <tr>
             <td>GitHub</td>
             <td>
-              <a href="https://github.com/matt-block">matt-block</a>
+              <a href="https://github.com/matt-block" target="_blank">
+                matt-block
+              </a>
             </td>
           </tr>
           <tr>
             <td>Stack Overflow</td>
             <td>
-              <a href="https://stackoverflow.com/u/1887860">Profile</a>
+              <a href="https://stackoverflow.com/u/1887860" target="_blank">
+                Profile
+              </a>
             </td>
           </tr>
           <tr>
             <td>Twitter</td>
             <td>
-              <a href="https://twitter.com/matt__block">@matt__block</a>
+              <a href="https://twitter.com/matt__block" target="_blank">
+                @matt__block
+              </a>
             </td>
           </tr>
           <tr>
             <td>LinkedIn</td>
             <td>
-              <a href="https://www.linkedin.com/in/mateibogdanradu/">Profile</a>
+              <a
+                href="https://www.linkedin.com/in/mateibogdanradu/"
+                target="_blank"
+              >
+                Profile
+              </a>
             </td>
-          </tr>
-          <tr>
-            <td />
-            <td />
           </tr>
         </tbody>
       </table>
